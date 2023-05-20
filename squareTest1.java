af7f0208b72ec945bda9aec9ef6f64ef98f7e6b2
vaish2303 <121894249+vaish2303@users.noreply.github.com>
2023-05-20 18:55:40 +0530

    Add files via upload

---
+++ squareTest1.java
@@ -0,0 +1,14 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class squareTest1 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
